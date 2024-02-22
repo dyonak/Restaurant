2f8b827f367b9956123e8555325cae4deaf420d0
Dustin Yonak <dyonak@gmail.com>
2024-02-21 18:32:44 -0600

    Initial structure setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: "Peter's Pickle Palace",
+        filename: 'index.html',
+        inject: 'body',
+    }),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+        },
+    ],
+  },
+};
