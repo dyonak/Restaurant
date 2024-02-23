85c402e03d27ad4376ae738506068cdc1cdbf5d0
Dustin Yonak <dyonak@gmail.com>
2024-02-23 10:15:25 -0600

    Home page loading by default on site entry or refresh

--- src/index.js
+++ src/index.js
@@ -10,15 +10,21 @@ let PageController = (function () {
   //Get the buttons from nav and add event listeners
   function setupPage() {
     buttons.forEach((button) => {
-      button.addEventListener("click", (event) => pageUpdate(event));
+      button.addEventListener("click", (event) => pageUpdate(event.target));
     });
+    //Call page update to load the homepage since this is the initial page load
+    //NOTE: This will mean refresh ALWAYS takes users back to this page
+    pageUpdate(document.querySelector(".button1"));
   }
 
   //Called from nav buttons, first clear the page, then call to load the new content based on clicked button
   function pageUpdate(event) {
+    //Update buttons to indicate current page
     buttons.forEach((button) => button.classList.remove("current"));
-    event.target.classList.add("current");
-    let requestedPage = event.target.textContent;
+    event.classList.add("current");
+
+    //Setup new page content
+    let requestedPage = event.textContent;
     clearPage();
     loadPage(requestedPage);
   }
