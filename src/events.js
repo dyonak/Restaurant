b3e5cd47a018de589f4af8f0827dbdc159678655
Dustin Yonak <dyonak@gmail.com>
2024-02-24 22:06:40 -0600

    All page content created, pretty basic

--- src/events.js
+++ src/events.js
@@ -1,10 +1,24 @@
 const showEvents = () => {
-  const newDiv = document.createElement("div");
+  const container = document.createElement("div");
+  container.classList.add("mainContent");
+
+  let newDiv = document.createElement("div");
   newDiv.classList.add("pagetitle");
 
-  const newContent = document.createTextNode("Events");
+  let newContent = document.createTextNode("Events");
   newDiv.appendChild(newContent);
-  return newDiv;
+
+  container.appendChild(newDiv);
+
+  let eventsDiv = document.createElement("div");
+  eventsDiv.classList.add("events");
+
+  eventsDiv.innerHTML =
+    "<ul><li>March 3, 2024 - Mezcal Tasting</li><li>March 26, 2024 - Rare Bottle Swap Meet</li><li>April 13, 2024 - Agave Cultivation</li><li>April 28, 2024 - Tequila Region Culture</li></ul>";
+
+  container.appendChild(eventsDiv);
+
+  return container;
 };
 
 export { showEvents };
--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,24 @@
 const showMenu = () => {
-  const newDiv = document.createElement("div");
+  const container = document.createElement("div");
+  container.classList.add("mainContent");
+
+  let newDiv = document.createElement("div");
   newDiv.classList.add("pagetitle");
 
-  const newContent = document.createTextNode("Menu");
+  let newContent = document.createTextNode("Menu");
   newDiv.appendChild(newContent);
-  return newDiv;
+
+  container.appendChild(newDiv);
+
+  let menuDiv = document.createElement("div");
+  menuDiv.classList.add("menu");
+
+  menuDiv.innerHTML =
+    "<ul><li>Conchica Blue</li><li>Juan Cusquena 20 anos</li><li>Madre Vizuvo (Mezcal)</li><li>Don Pimon</li><li>Consuela Serpente</li></ul>";
+
+  container.appendChild(menuDiv);
+
+  return container;
 };
 
 export { showMenu };
