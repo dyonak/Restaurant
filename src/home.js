bbf8d21ca0ee39b168135ab2ee1cfd01ec56f301
Dustin Yonak <dyonak@gmail.com>
2024-02-24 19:34:44 -0600

    Header css fine tuned, beginning content for main page in place.
    Main page needs more styling, maybe an image or two.

--- src/home.js
+++ src/home.js
@@ -1,10 +1,26 @@
 const showHome = () => {
-  const newDiv = document.createElement("div");
+  const container = document.createElement("div");
+  container.classList.add("mainContent");
+
+  let newDiv = document.createElement("div");
   newDiv.classList.add("pagetitle");
 
-  const newContent = document.createTextNode("Home");
+  let newContent = document.createTextNode("Home");
   newDiv.appendChild(newContent);
-  return newDiv;
+
+  container.appendChild(newDiv);
+
+  newDiv = document.createElement("p");
+  newDiv.classList.add("aboutpara");
+
+  newContent = document.createTextNode(
+    "Tequileria welcomes you to explore the finest collection of tequilas and mezcals this side of the Rio Grande. At Tequileria, we invite you to discover and enjoy all the traditions and flavors Mexico has to offer. Buen provecho!"
+  );
+  newDiv.appendChild(newContent);
+
+  container.appendChild(newDiv);
+  console.log(container);
+  return container;
 };
 
 export { showHome };
